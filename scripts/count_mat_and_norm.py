f1037e5f925a184e3ce61d32e05804774f44098d
fernando <d.fernando.fg@gmail.com>
2023-04-04 09:13:47 +0100

    Updated

--- scripts/count_mat_and_norm.py
+++ scripts/count_mat_and_norm.py
@@ -1,29 +1,28 @@
+import argparse
 
 def main():
 	parser = argparse.ArgumentParser()
-	parser.add_argument("-s","--sequence", help="sequence in fata format")
-	parser.add_argument("-f","--forward", help="forward primer", type = str)
-	parser.add_argument("-r","--reverse", help="reverse primer", type = str)
-	parser.add_argument("-m","--mismatches", default = 0, help="number of mismatches", type = str)
-	parser.add_argument("--minlength", default = 100, help="minimum product length", type = int)
-	parser.add_argument("--maxlength", default = 1000, help="maximum product length", type = int)
+	parser.add_argument("-c","--counts", help="counts file from featureCounts")
+	parser.add_argument("-l","--16S_lenght", help="mean length of 16S sequences present in library", type = float)
+	parser.add_argument("-n","--16S_number", help="number of 16S sequences present in library", type = float)
 	return parser.parse_args()
 
 args = main()
 
+counts = args.counts()
 
-
-with open('SRR6713625.counts', "r") as f:
-    for x in range(2):
-        next(f)
-    for line in f:
-        line = line.split("\t")
-        length, counts = float(line[5]), float(line[6])
-        try:
-            norm = (length/counts)/400
-        except ZeroDivisionError:
-            norm = 0
-        print(norm)
+if
+    with open(counts, "r") as f:
+        for x in range(2):
+            next(f)
+        for line in f:
+            line = line.split("\t")
+            length, counts = float(line[5]), float(line[6])
+            try:
+                norm = (length/counts)/400
+            except ZeroDivisionError:
+                norm = 0
+            print(norm)
         
 
 
